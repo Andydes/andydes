181ce70e488d65664a75c8e483510ba8b13166be
Andrea <andreamariangelap10@gmail.com>
2020-07-08 18:32:59 -0300

    redirection

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+header("Location: https://www.colourandy.com/"); /* Redirección del navegador */
+
+/* Asegurándonos de que el código interior no será ejecutado cuando se realiza la redirección. */
+exit;
+?>
